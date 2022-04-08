ff391c160076d02f65d07c70856a0c03a4c15864
Luis M. Correa <luis.m.correa.perez@intel.com>
2022-04-08 12:08:24 -0500

    Update get_build_microsoft_libs.py

--- utils/get_build_microsoft_libs.py
+++ utils/get_build_microsoft_libs.py
@@ -6,8 +6,8 @@ from msrest.authentication import BasicAuthentication
 from setup_logger import logger
 # otro commit 
 
-#conflict7
-#conflict8
+#conflict9
+#conflict10
 class AzureBuildInfo:
     pass
 
